70f316cc9f5e08c1cd8c64775646cf722905717f
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-12-01 15:39:56 -0300

    initial commit

---
+++ src/main/java/br/com/eduardowanderley/personregistration/PersonRegistrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardowanderley.personregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegistrationApplication.class, args);
+	}
+
+}
