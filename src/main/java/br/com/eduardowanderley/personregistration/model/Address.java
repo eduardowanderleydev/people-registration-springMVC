546c023c71a777a138ad195bafb4e02249595856
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-12-01 15:51:59 -0300

    Starting to implement model layer

---
+++ src/main/java/br/com/eduardowanderley/personregistration/model/Address.java
@@ -0,0 +1,66 @@
+package br.com.eduardowanderley.personregistration.model;
+
+public class Address {
+    private String cep;
+
+    private String rua;
+
+    private String bairro;
+
+    private String cidade;
+
+    private String uf;
+
+    private String ibge;
+
+    public Address(){
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getIbge() {
+        return ibge;
+    }
+
+    public void setIbge(String ibge) {
+        this.ibge = ibge;
+    }
+}
