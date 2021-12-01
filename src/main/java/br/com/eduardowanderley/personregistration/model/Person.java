525e1a44f850c4c8f60185d89d1e289a8aa7a42d
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-12-01 15:52:07 -0300

    Starting to implement model layer

---
+++ src/main/java/br/com/eduardowanderley/personregistration/model/Person.java
@@ -0,0 +1,100 @@
+package br.com.eduardowanderley.personregistration.model;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Person {
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private Integer age;
+
+    private String gender;
+
+    private LocalDate birthDate;
+
+    List<Phone> phones;
+
+    private Address address;
+
+    private Occupation occupation;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Occupation getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(Occupation occupation) {
+        this.occupation = occupation;
+    }
+}
---
+++ src/main/java/br/com/eduardowanderley/personregistration/model/Phone.java
@@ -0,0 +1,45 @@
+package br.com.eduardowanderley.personregistration.model;
+
+public class Phone {
+
+    private Long id;
+    private String number;
+    private String type;
+
+    private Person person;
+
+    public Phone(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
